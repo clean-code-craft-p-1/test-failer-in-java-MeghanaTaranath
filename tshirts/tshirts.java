13b824e5444a17dc03bd7c8706ce2c3dd6451f6f
MeghanaTaranath <mtaranath91@gmail.com>
2025-07-10 12:50:11 +0530

    tshirts.java

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -8,8 +8,4 @@ public class tshirts {
             return "L";
         }
     }
-
-    public static void main(String[] args) {
-        System.out.println("Use TShirtsTest for testing.");
-    }
 }
