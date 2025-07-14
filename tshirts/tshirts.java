1a823639e5383db833b1b40c1b43f08310fda4a6
MeghanaTaranath <mtaranath91@gmail.com>
2025-07-14 12:43:03 +0530

    Update tshirts.java

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -1,5 +1,12 @@
 public class tshirts {
-    public static String size(int cms) {
+
+    /**
+     * Returns the size category based on chest measurement in cm.
+     * 
+     * @param cms chest size in centimeters
+     * @return "S", "M", or "L"
+     */
+    public static String getSize(int cms) {
         if (cms < 38) {
             return "S";
         } else if (cms > 38 && cms < 42) {
